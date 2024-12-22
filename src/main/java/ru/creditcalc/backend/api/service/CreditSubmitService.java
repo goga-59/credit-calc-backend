ed15668c8a329a7e5283f85191d1d8b671da05d1
goga-59 <egsad2004@gmail.com>
2024-12-22 20:48:43 +0500

    change reasons for CreditSubmitService

--- src/main/java/ru/creditcalc/backend/api/service/CreditSubmitService.java
+++ src/main/java/ru/creditcalc/backend/api/service/CreditSubmitService.java
@@ -22,6 +22,7 @@ public class CreditSubmitService {
     private final double MAX_LOAN_AMOUNT_TO_SALARY_RATIO = 5D;
     private final int MIN_AGE = 18;
     private final List<EmploymentType> RESTRICTED_EMPLOYMENT_TYPES = List.of(EmploymentType.UNEMPLOYED);
+    private static final String rejection = "Заявка отклонена. Причина: ";
 
     public List<String> submitLoan(CreditSubmitDto dto) {
         creditRepository.save(new Credit(dto));
@@ -29,21 +30,21 @@ public class CreditSubmitService {
         short age = calculateAge(dto.getBirthDate());
 
         if (dto.getSalary() < MIN_SALARY)
-            return List.of("Rejected", "Salary");
+            return List.of("Rejected", rejection + "Ваш доход ниже минимального для получения кредита.");
 
         if (age < MIN_AGE)
-            return List.of("Rejected", "Age");
+            return List.of("Rejected", rejection + "Ваш возраст должен быть не менее 18 лет для получения кредита.");
 
         if (RESTRICTED_EMPLOYMENT_TYPES.contains(dto.getEmploymentType()))
-            return List.of("Rejected", "Employment type");
+            return List.of("Rejected", rejection + "Кредит не может быть предоставлен с текущим типом занятости.");
 
         if (dto.getLoanAmount() > MAX_LOAN_AMOUNT_TO_SALARY_RATIO * dto.getSalary())
-            return List.of("Rejected", "Loan amount");
+            return List.of("Rejected", rejection + "Запрашивамая сумма кредита превышает допустимый лимит относительно вашего дохода.");
 
         if (dto.getMaritalStatus() == MaritalStatus.DIVORCED || dto.getMaritalStatus() == MaritalStatus.WIDOWED)
-            return List.of("Approved with limits", "Marital status");
+            return List.of("Approved with limits", "Мы можем предложить вам ограниченные условия кредита.");
 
-        return List.of("Approved", "Everything is fine");
+        return List.of("Approved", "Кредит одобрен.");
     }
 
     public short calculateAge(LocalDate birthDate) {
