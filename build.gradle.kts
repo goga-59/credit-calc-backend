6e31c3639d2b21ed70cd76d0d3abeac2f33595e8
goga-59 <egsad2004@gmail.com>
2025-01-23 09:19:07 +0500

    update build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,23 @@
 plugins {
     java
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.google.cloud.tools.jib") version "3.4.4"
 }
 
-group = "ru.creditcalc"
+group = "ru.creditcalc.backend"
 version = "0.0.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
 }
