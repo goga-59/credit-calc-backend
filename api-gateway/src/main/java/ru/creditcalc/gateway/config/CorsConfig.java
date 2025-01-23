98a32ca2e61450aefeaba8a414f1e9b30f176899
goga-59 <egsad2004@gmail.com>
2025-01-23 09:17:20 +0500

    remove cors config

--- api-gateway/src/main/java/ru/creditcalc/gateway/config/CorsConfig.java
+++ api-gateway/src/main/java/ru/creditcalc/gateway/config/CorsConfig.java
@@ -1,15 +1,13 @@
-package ru.creditcalc.discovery.config;
+package ru.creditcalc.gateway.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@Configuration
 public class CorsConfig {
 
     @Value("${server.cors.allowed-origins-pattern}")
